775e6d95064c8ab627f992a9317c7df37a084049
Duflot Patrick <c194437@chulg>
2023-03-24 15:56:36 +0100

    extract script

--- sample.py
+++ sample.py
@@ -1,11 +1,35 @@
 from dicompylercore import dicomparser, dvh, dvhcalc
-absdvh = dvhcalc.get_dvh("../data/export_short/RS.dcm", "../data/export_short/RD.dcm", 17)
-absdvh.describe()
+import jsons
+
 dp = dicomparser.DicomParser("../data/export_short/RS.dcm")
 structures = dp.GetStructures()
+infos = []
+for structure in structures.values():
+    if (structure['type']!='MARKER') and (structure['type']!='CONTROL'):
+        infos.append({ "id": structure['id'], "name": structure['name'], "type": structure['type'], "empty": structure['empty']})
+str = jsons.dumps(infos)
+f = open("../data/structures.json", "w")
+f.write(str)
+f.close()
+
+for info in infos:
+    dvhabs = dvhcalc.get_dvh("../data/export_short/RS.dcm", "../data/export_short/RD.dcm", info['id'])
+    dvhreldose = dvhabs.relative_dose(6400)
+    dvhrelvol = dvhabs.relative_volume
+    dvhinfo = { "bins": dvhrelvol.bins, "counts": dvhrelvol.counts, "dose_units": dvhrelvol.dose_units, "volume_units": dvhrelvol.volume_units, 
+               "volumeCm3": dvhabs.volume, "maxDose": dvhabs.max, "minDose": dvhabs.min, "meanDose": dvhabs.mean,
+               "D100": dvhabs.D100, "D98": dvhabs.D98, "D95": dvhabs.D95, "D2cc": dvhabs.D2cc
+              }
+    str = jsons.dumps(dvhinfo)
+    f = open("../data/dvhinfo_%d.json" % info['id'], "w")
+    f.write(str)
+    f.close()
+quit()
+
+absdvh = dvhcalc.get_dvh("../data/export_short/RS.dcm", "../data/export_short/RD.dcm", 17)
+absdvh.describe()
 dvhrel = absdvh.relative_dose(6400)
 dvhrel.describe()
-import jsons
 str = jsons.dumps(absdvh.__dict__)
 f = open("../data/absdvh.json", "w")
 f.write(str)
